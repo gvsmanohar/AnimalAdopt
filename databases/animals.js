77c3fc896ef61dd855ff0015c0e7faee2b01c662
Venkata Sai Manohar Gedela <63358674+gvsmanohar@users.noreply.github.com>
2022-03-21 08:41:46 +0000

    Add files via upload

---
+++ databases/animals.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+let mongoDB = 'mongodb://localhost:27017/animals';
+
+mongoose.Promise = global.Promise;
+
+mongoose.Promise = global.Promise;
+try {
+    connection = mongoose.connect(mongoDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        checkServerIdentity: false,
+    });
+    console.log('connection to mongodb worked!');
+
+// db.dropDatabase();
+
+} catch (e) {
+    console.log('error in db connection: ' + e.message);
+}
